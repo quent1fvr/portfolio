6c309b67e8fb4b3eb833fdd8d9b3bdf42f8f5acc
Quentin Fever <quentinfever@rn-dynamic-187-003.wless.cranfield.ac.uk>
2023-03-03 12:20:23 +0000

    Small changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ import About from 'components/About';
 import Skills from 'components/Skills';
 import Projects from 'components/Projects';
 import Contact from 'components/Contact';
-import ErrorBoundary from '../components/ErrorBoundary'
 
 
 export default function Home() {
