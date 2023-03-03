d86c3156df9bb508012c2cec08b969f742a926cb
Quentin Fever <quentinfever@rn-dynamic-187-003.wless.cranfield.ac.uk>
2023-03-03 12:35:56 +0000

    Quick fix attempt

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 
 import Navbar from "components/Navbar";
-import Main from "components/Main";
 
 import "../styles/globals.css";
 
@@ -9,7 +8,6 @@ function MyApp({ Component, pageProps }) {
   return (
   <>
     <Navbar/>
-    <Main />
 
     <Component {...pageProps} />
   </> 
