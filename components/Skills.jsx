9d7cafc4a200f058e63bc10cc81d21f7094aa625
Quentin Fever <quentinfever@ed-dynamic-155-026.wless.cranfield.ac.uk>
2023-03-05 23:28:33 +0000

    quick fix

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -15,7 +15,7 @@ function Skills() {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'> 
                         <div className='m-auto'>
-                            <Image src='/public/assets/skills/python.png' width='64' height='64' alt='/' />
+                            <Image src='public/assets/skills/python.png' width='64' height='64' alt='/' />
                         </div>
                         <div className='flex flex-col items-center justify-center'>
                             <h3>PYTHON</h3>
