fc5c1d23b7d263f0dd7e5f02b179e3df5b485953
Quentin Fever <quentinfever@rn-dynamic-187-007.wless.cranfield.ac.uk>
2023-03-27 14:46:54 +0100

    Analytics package added2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@
 import Navbar from "components/Navbar";
 
 import "../styles/globals.css";
-import {Analytics} from '@vercel/analytics'
+import {Analytics} from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }) {
   return (
